05ef4ed88578ed885191b8935f1f6e1c0edf4b03
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-02 13:49:18 -0300

    Import new external files

--- init.lua
+++ init.lua
@@ -1,20 +1,21 @@
 -- Colors
 vim.opt.termguicolors = true
-vim.cmd [[
+vim.cmd([[
     colorscheme onedark
     filetype plugin indent on
     syntax on
     highlight clear StatusLine
-]]
+]])
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 2
 -- Paths
-vim.o.path = vim.o.path .. '**'
+vim.o.path = vim.o.path .. "**"
 vim.opt.swapfile = false
 vim.opt.undofile = true
-vim.cmd [[ set undodir=~/.config/nvim/undodir ]]
-vim.opt.mouse = 'a'
+vim.cmd([[ set undodir=~/.config/nvim/undodir ]])
+vim.opt.mouse = "a"
+vim.cmd([[ set guicursor= ]])
 -- Tabulation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
@@ -29,23 +30,31 @@ vim.opt.confirm = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.wrap = false
-vim.opt.foldmethod = 'marker'
+vim.opt.foldmethod = "marker"
 -- Fill characters
 vim.opt.list = false
-vim.opt.listchars:append('tab:›-,space:·,trail:_,eol:↲')
-vim.opt.fillchars:append('fold: ')
+vim.opt.listchars:append("tab:›-,space:·,trail:_,eol:↲")
+vim.opt.fillchars:append("fold: ")
 -- Spelling
 vim.opt.spell = false
-vim.opt.spelllang = 'pt_br,en'
+vim.opt.spelllang = "pt_br,en"
 -- Insert mode menu
-vim.opt.complete:append('kspell')
-vim.opt.completeopt = 'menuone,longest'
-vim.opt.shortmess:append('c')
+vim.opt.complete:append("kspell")
+vim.opt.completeopt = "menuone,longest"
+vim.opt.shortmess:append("c")
 -- Extern files
-require 'plug-manager'
-require 'syntaxhl'
-require 'autocmds'
-require 'mappings'
-require 'file-manager'
-require 'utils'
+require("plug-manager")
+require("syntaxhl")
+require("autocmds")
+require("mappings")
+require("file-manager")
+require("utils")
+require("lsp")
+require("snippets")
+require("complete")
 
+vim.diagnostic.config({
+	float = { source = "always", border = border },
+	virtual_text = false,
+	signs = true,
+})
