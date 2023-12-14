22fced737eef15a6e8ff1ba1fb0ef7325e483681
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-14 16:23:16 -0300

    Add lua lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,8 @@
 -- Setup language servers.
 local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({})
+lspconfig.lua_ls.setup({})
 lspconfig.tsserver.setup({})
+lspconfig.pyright.setup({})
 lspconfig.rust_analyzer.setup({
 	-- Server-specific settings. See `:help lspconfig-setup`
 	settings = {
