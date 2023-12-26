2bfe3ca9039a64b535f5a7dd6d7e98854d4c2a58
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-26 14:23:40 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,5 @@
+require("plugins")
 require("base")
 require("mappings")
---require("plug-manager")
---require("syntaxhl")
---require("autocmds")
---require("file-manager")
---require("utils")
---require("lsp")
---require("snippets")
---require("complete")
---
---vim.diagnostic.config({
---	float = { source = "always", border = border },
---	virtual_text = false,
---	signs = true,
---})
+require("autocmds")
+require("syntaxhl")
