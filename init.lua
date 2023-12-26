c47e2380d8fcde99be667389926fc618d9ac50a5
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-26 12:20:38 -0300

    Create base.lua

--- init.lua
+++ init.lua
@@ -1,61 +1,16 @@
--- Colors
-vim.opt.termguicolors = true
-vim.cmd([[
-    colorscheme onedark
-    filetype plugin indent on
-    syntax on
-    highlight clear StatusLine
-    hi Comment cterm=italic gui=italic
-]])
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.scrolloff = 2
--- Paths
-vim.o.path = vim.o.path .. "**"
-vim.opt.swapfile = false
-vim.opt.undofile = true
-vim.cmd([[ set undodir=~/.config/nvim/undodir ]])
-vim.opt.mouse = "a"
-vim.cmd([[ set guicursor= ]])
--- Tabulation
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- General
-vim.opt.smartindent = true
-vim.opt.confirm = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.wrap = false
-vim.opt.foldmethod = "marker"
--- Fill characters
-vim.opt.list = false
-vim.opt.listchars:append("tab:›-,space:·,trail:_,eol:↲")
-vim.opt.fillchars:append("fold: ")
--- Spelling
-vim.opt.spell = false
-vim.opt.spelllang = "pt_br,en"
--- Insert mode menu
-vim.opt.complete:append("kspell")
-vim.opt.completeopt = "menuone,longest"
-vim.opt.shortmess:append("c")
--- Extern files
-require("plug-manager")
-require("syntaxhl")
-require("autocmds")
+require("base")
 require("mappings")
-require("file-manager")
-require("utils")
-require("lsp")
-require("snippets")
-require("complete")
-
-vim.diagnostic.config({
-	float = { source = "always", border = border },
-	virtual_text = false,
-	signs = true,
-})
+--require("plug-manager")
+--require("syntaxhl")
+--require("autocmds")
+--require("file-manager")
+--require("utils")
+--require("lsp")
+--require("snippets")
+--require("complete")
+--
+--vim.diagnostic.config({
+--	float = { source = "always", border = border },
+--	virtual_text = false,
+--	signs = true,
+--})
---
+++ lua/base.lua
@@ -0,0 +1,58 @@
+-- Base
+local g = vim.g
+g.mapleader = ","
+
+local o = vim.o
+local opt = vim.opt
+local cmd = vim.cmd
+
+o.termguicolors = true
+
+o.scrolloff = 8
+
+o.number = true
+o.numberwidth = 2
+o.cursorline = true
+
+o.expandtab = true
+o.autoindent = true
+o.wrap = false
+o.textwidth = 79
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1
+o.list = false
+o.listchars = "tab:›-,space:·,trail:_,eol:↲"
+
+o.ignorecase = true
+o.smartcase = true
+
+o.backup = false
+o.writebackup = false
+o.undofile = true
+cmd([[ set undodir=~/.config/nvim/undodir ]])
+o.swapfile = false
+
+o.splitbelow = true
+o.splitright = true
+
+opt.mouse = "a"
+
+cmd([[
+    colorscheme minimalist
+    highlight clear StatusLine
+]])
+
+cmd([[
+    syntax on
+    filetype plugin indent on
+]])
+
+opt.confirm = true
+
+o.foldmethod = "marker"
+o.fillchars = "fold: "
+
+---- Spelling
+opt.spell = false
+opt.spelllang = "pt_br,en"
