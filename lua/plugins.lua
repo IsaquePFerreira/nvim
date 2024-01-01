0cd7290970f6a1dfa21e4678e1e2dfd63d9a313e
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-01-01 15:35:12 -0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,13 +3,4 @@ local Plug = vim.fn["plug#"]
 vim.call("plug#begin", "~/.config/nvim/plugged")
 Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
 Plug("norcalli/nvim-colorizer.lua")
--- Plug("gosukiwi/vim-zensnippets")
--- Plug("neovim/nvim-lspconfig")
--- Plug("L3MON4D3/LuaSnip", { ["tag"] = "v2.*", ["do"] = ":make install_jsregexp" })
--- Plug("hrsh7th/cmp-nvim-lsp")
--- Plug("hrsh7th/cmp-buffer")
--- Plug("hrsh7th/cmp-path")
--- Plug("hrsh7th/nvim-cmp")
--- Plug("saadparwaiz1/cmp_luasnip")
--- Plug "rafamadriz/friendly-snippets"
 vim.call("plug#end")
